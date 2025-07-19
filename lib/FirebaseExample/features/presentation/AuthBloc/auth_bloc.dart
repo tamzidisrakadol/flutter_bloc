53d72e503500893d21f0f5bdca6867e1f032e46a
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-07-20 04:01:30 +0600

    Check the login functionality is also working

--- lib/FirebaseExample/features/presentation/AuthBloc/auth_bloc.dart
+++ lib/FirebaseExample/features/presentation/AuthBloc/auth_bloc.dart
@@ -6,24 +6,35 @@ import 'package:flutter_b_sm/FirebaseExample/features/domain/Repository/AuthRepo
 import '../../domain/Entities/UserEntity.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-
   final AuthRepository authRepository;
 
   AuthBloc(this.authRepository) : super(AuthStateInitial()) {
     on<SignUpRequest>(_onSignUpRequest);
   }
 
-  Future<void> _onSignUpRequest(SignUpRequest event, Emitter<AuthState> emit) async {
+  Future<void> _onSignUpRequest(
+    SignUpRequest event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(AuthStateLoading());
-    final result = await authRepository.signUpWithEmailAndPassword(email: event.email, password: event.password);
+
+    //register
+     final result = await authRepository.signUpWithEmailAndPassword(email: event.email, password: event.password);
+
+     //sign in
+    // final result = await authRepository.loginUserWithEmailAndPassword(
+    //   event.email,
+    //   event.password,
+    // );
 
     //must use await to use fold
     result.fold(
-          (error) => emit(AuthError(error.toString())),
-          (user) => emit(AuthStateSuccess(user, "Success")),
+      (error) => emit(AuthError(error.toString())),
+      (user) => emit(AuthStateSuccess(user, "Success")),
     );
   }
 }
